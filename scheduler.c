8477a1806e253b787e05fee6b41ac05426c8bd85
Daniel Chi <b06902078@ntu.edu.tw>
2019-04-27 18:54:53 +0800

    modify previous code

--- scheduler.c
+++ scheduler.c
@@ -18,31 +18,31 @@ char N[1024][32]; //names of the processes
 int R[1024]; //ready time
 int T[1024]; //execution time
 
-int policy;
-
 int main(int argc, char *argv[]) {
     
+    int policy = 0;
+    
     //INPUT
-    scanf("%s%d", S, &Nn);
+    scanf("%s", S);
     
     if(strcmp(S, "FIFO") == 0) {
-        policy = 0;
-    }
-    else if(strcmp(S, "RR") == 0) {
         policy = 1;
     }
-    else if(strcmp(S, "SJF") == 0) {
+    else if(strcmp(S, "RR") == 0) {
         policy = 2;
     }
-    else if(strcmp(S, "PSJF") == 0) {
+    else if(strcmp(S, "SJF") == 0) {
         policy = 3;
     }
+    else if(strcmp(S, "PSJF") == 0) {
+        policy = 4;
+    }
     else {
-        fprintf(stderr. "Invalid scheduling policy.\n");
+        fprintf(stderr, "Invalid scheduling policy: %s\n", S);
         exit(0);
     }
     
-    
+    scanf("%d", &Nn);
     for(int i = 0; i < Nn; i++) {
         scanf("%s%d%d", N[i], &R[i], &T[i]);
     }
