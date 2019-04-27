ba5de910bd23587c7571f0dd8956d982ec03f662
Daniel Chi <b06902078@ntu.edu.tw>
2019-04-27 18:30:24 +0800

    add code in scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -18,10 +18,31 @@ char N[1024][32]; //names of the processes
 int R[1024]; //ready time
 int T[1024]; //execution time
 
+int policy;
+
 int main(int argc, char *argv[]) {
     
     //INPUT
     scanf("%s%d", S, &Nn);
+    
+    if(strcmp(S, "FIFO") == 0) {
+        policy = 0;
+    }
+    else if(strcmp(S, "RR") == 0) {
+        policy = 1;
+    }
+    else if(strcmp(S, "SJF") == 0) {
+        policy = 2;
+    }
+    else if(strcmp(S, "PSJF") == 0) {
+        policy = 3;
+    }
+    else {
+        fprintf(stderr. "Invalid scheduling policy.\n");
+        exit(0);
+    }
+    
+    
     for(int i = 0; i < Nn; i++) {
         scanf("%s%d%d", N[i], &R[i], &T[i]);
     }
