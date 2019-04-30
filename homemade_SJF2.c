893940c7ab732eb7eb5c515bd83dfc9c8beceb2b
Daniel Chi <b06902078@ntu.edu.tw>
2019-05-01 07:42:44 +0800

    update

--- homemade_SJF2.c
+++ homemade_SJF2.c
@@ -118,7 +118,10 @@ int main(int argc, char* argv[])
 				run = next;
                 running = 1;
                 proc[run].t_exec = 0;
-                waitpid(proc[run].pid, NULL, 0);
+                if(proc[run].pid != -1) {
+                    waitpid(proc[run].pid, NULL, 0);
+                    proc[run].pid = -1;
+                }
 			}
 		}
     
