7117bc0054b62e49a1b88056009e710f2d372e98
Daniel Chi <b06902078@ntu.edu.tw>
2019-05-01 07:58:21 +0800

    update

--- homemade_SJF2.c
+++ homemade_SJF2.c
@@ -16,113 +16,90 @@
 #include "functions.h"
 #define ever ;;
 
-static int numberOfTime, run, countFinishing;
-int numberOfProcess;
-int running = 0;
-
-void sig_child(int signum)
-{
-    static int finished = 0;
-    wait(NULL);
-    finished++;
-    running = 0;
-    run = -1;
-    if (finished == numberOfProcess) exit(0);
-}
+static int last,numberOfTime, run, countFinishing;
 
 int proc_wakeup(int pid)
 {
-	struct sched_param param;
-	param.sched_priority = 0;
-	int ret = sched_setscheduler(pid, SCHED_OTHER, &param);
-	if (ret < 0) {
-		//perror("sched_setscheduler");
-		return -1;
-	}
-	return ret;
+    struct sched_param param;
+    param.sched_priority = 0;
+    int ret = sched_setscheduler(pid, SCHED_OTHER, &param);
+    if (ret < 0) {
+        //perror("sched_setscheduler");
+        return -1;
+    }
+    return ret;
 }
 
 int proc_block(int pid)
 {
-	struct sched_param param;
-	param.sched_priority = 0;
-	int ret = sched_setscheduler(pid, SCHED_IDLE, &param);
-	if (ret < 0) 
-		return -1;
-	return ret;
+    struct sched_param param;
+    param.sched_priority = 0;
+    int ret = sched_setscheduler(pid, SCHED_IDLE, &param);
+    if (ret < 0)
+        return -1;
+    return ret;
 }
 
-
-int nextP(struct process *proc, int numberOfProcess, int numberOfTime)
+int nextP(struct process *proc, int numberOfProcess)
 {
     int ret = -1;
-	if(running == 1) {
-        	return ret;
-    	}
-    
-	for (int i = 0; i < numberOfProcess; i++) {
-        if(proc[i].t_ready > numberOfTime) {
-            //printf("return here\n");
-            break;
-        }
-        if(i == 0 && proc[i].t_exec != 0) {
-            //printf("return aqui\n");
-            return i;
-        }
-        ret = 0;
-        if(proc[i].t_exec < proc[ret].t_exec) {
+    for (int i = 0; i < numberOfProcess; i++) {
+        if (proc[i].pid == -1 || proc[i].t_exec == 0)
+            continue;
+        if (ret == -1 || proc[i].t_exec < proc[ret].t_exec)
             ret = i;
-        }
-	}
-    //printf("return ici\n");
-    //fprintf(stderr, "nextP %d!\n", ret);
-	return ret;
+    }
+    return ret;
 }
-
 
 int main(int argc, char* argv[])
 {
-
-	struct process *proc;
-
-	scanf("%d", &numberOfProcess);
+    int numberOfProcess;
+    struct process *proc;
+    
+    scanf("%d", &numberOfProcess);
     
     proc = take_tasks(numberOfProcess);
     
-  struct sigaction act;
-    act.sa_flags = 0;
-    act.sa_handler = sig_child;
-    sigfillset(&act.sa_mask);
-    sigaction(SIGCHLD, &act, NULL);
-
-	proc_wakeup(getpid());
-	numberOfTime = 0;
-	run = -1;
-	countFinishing = 0;
-
-	for(ever) {
-		for (int i = 0; i < numberOfProcess; i++) {
-			if (proc[i].t_ready == numberOfTime) {
+    for (int i = 0; i < numberOfProcess; i++)
+        proc[i].pid = -1;
+    
+    assign_cpu(getpid(), PARENT_CPU);
+    proc_wakeup(getpid());
+    numberOfTime = 0;
+    run = -1;
+    countFinishing = 0;
+    
+    for(ever) {
+        if (run != -1 && proc[run].t_exec == 0) {
+            waitpid(proc[run].pid, NULL, 0);
+            //printf("%s %d\n", proc[run].name, proc[run].pid);
+            run = -1;
+            countFinishing++;
+            if (countFinishing == numberOfProcess)
+                break;
+        }
+        for (int i = 0; i < numberOfProcess; i++) {
+            if (proc[i].t_ready == numberOfTime) {
                 create_proc(&proc[i].pid, proc[i].name, i, proc[i].t_exec);
-				proc_block(proc[i].pid);
-			}
-
-		}
-    
-		int next = nextP(proc, numberOfProcess, numberOfTime);
-		if (next != -1) {
-			if (run != next) {
-                //fprintf(stderr, "I choose you %s!\n", proc[next].name);
-				proc_wakeup(proc[next].pid);
-				//proc_block(proc[run].pid);
-				run = next;
-                running = 1;
+                proc_block(proc[i].pid);
+            }
+            
+        }
+        int next = nextP(proc, numberOfProcess);
+        if (next != -1) {
+            if (run != next) {
+                proc_wakeup(proc[next].pid);
+                proc_block(proc[run].pid);
+                run = next;
                 proc[run].t_exec = 0;
-			}
-		}
-    
-		run_unit_time();
-		numberOfTime++;
-	}
-	return 0;
+                last = numberOfTime;
+            }
+        }
+        run_unit_time();
+        //if (run != -1)
+          //  proc[run].t_exec--;
+        numberOfTime++;
+    }
+    return 0;
 }
