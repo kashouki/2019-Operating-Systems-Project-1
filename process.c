cf48be88ddcdfda3513a96ffc84ca6af23b87b6a
Daniel Chi <b06902078@ntu.edu.tw>
2019-05-01 06:00:58 +0800

    update

--- process.c
+++ process.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
     }
     clock_gettime(CLOCK_REALTIME, &t_finish);
     tiempo_terminar = syscall(352);
-    syscall(351, pid, start_time, end_time, 11111);
+    syscall(351, pid, tiempo_empezar, tiempo_terminar, 11111);
     char dmesg_buff[512];
     sprintf(dmesg_buff, "[project1] %d %lu.%09lu %lu.%09lu\n", pid, t_start.tv_sec, t_start.tv_nsec, t_finish.tv_sec, t_finish.tv_nsec);
     syscall(315, dmesg_buff);
