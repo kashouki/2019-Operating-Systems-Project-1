767d3c35b668c1dc1232dd803ac5907145a577dc
Daniel Chi <b06902078@ntu.edu.tw>
2019-04-30 22:07:16 +0800

    update

--- process.c
+++ process.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
     
     char dmesg_buff[512];
     sprintf(dmesg_buff, "[project1] %d %lu.%09lu %lu.%09lu\n", pid, t_start.tv_sec, t_start.tv_nsec, t_finish.tv_sec, t_finish.tv_nsec);
-    syscall(PRINTK, dmesg_buff);
+    //syscall(PRINTK, dmesg_buff);
     
     exit(0);
 }
