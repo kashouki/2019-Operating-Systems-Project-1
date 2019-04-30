a1af6ab1cb3a5721840036abdb720abbad9c41f7
Daniel Chi <b06902078@ntu.edu.tw>
2019-05-01 05:59:44 +0800

    update

--- process.c
+++ process.c
@@ -27,17 +27,21 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "%s %d\n", name, pid);
     
     struct timespec t_start, t_finish;
+    long tiempo_empezar, tiempo_terminar;
     
     clock_gettime(CLOCK_REALTIME, &t_start);
+    tiempo_empezar = syscall(352);
 
     for(int i = 0; i < t_exec; i++) {
         run_unit_time();
     }
     clock_gettime(CLOCK_REALTIME, &t_finish);
-    
+    tiempo_terminar = syscall(352);
+    syscall(351, pid, start_time, end_time, 11111);
     char dmesg_buff[512];
     sprintf(dmesg_buff, "[project1] %d %lu.%09lu %lu.%09lu\n", pid, t_start.tv_sec, t_start.tv_nsec, t_finish.tv_sec, t_finish.tv_nsec);
     syscall(315, dmesg_buff);
+    syscall(351, dmesg_buff);
     fprintf(stderr, "%s", dmesg_buff);
     
     exit(0);
