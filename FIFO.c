d209f2c8722a8bec71db6d6125d1db8f0e408e3f
Curchin <49954678+Curchin@users.noreply.github.com>
2019-04-30 21:38:04 +0800

    Update FIFO.c

--- FIFO.c
+++ FIFO.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <sched.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include "functions.h"
 
 int N, nextproc = 0, workingon = 0;
+process* proc;
 
 void sig_child(int signum)
 {
@@ -14,7 +18,7 @@ void sig_child(int signum)
     if (finished == N) exit(0);
 }
 
-void change_priority(pid_t pid) {
+void change_priority() {
     if (running == 0){
         set_priority(proc[nextproc].pid, SCHED_FIFO, HIGH_PRIORITY);
         running = 1;
@@ -29,30 +33,26 @@ int main(void) {
     // init
 
     scanf("%d", &N);
-    process* proc;
+    
     proc = take_tasks(N);
     
     for (int i = 0; i < nproc; i++){
         proc[i].pid = -1;
     }
     
-    while (1) {
-        struct sigaction act;
-        act.sa_flags = 0;
-        act.sa_handler = sig_child;
-        sigfillset(&act.sa_mask);
-        sigaction(SIGCHLD, &act, NULL);
-    }
-    
+    struct sigaction act;
+    act.sa_flags = 0;
+    act.sa_handler = sig_child;
+    sigfillset(&act.sa_mask);
+    sigaction(SIGCHLD, &act, NULL);
     
     //F IFO
     for (int time = 0, int i = N; i > 0; time++) {
         change_priority();
         
         while (nextproc < N && time == proc[nextproc].t_ready) {
-            (nextproc < N && time == proc[nextproc].t_ready
             
-            create_process(proc[nextproc].pid, proc[nextproc].name, nextproc, proc[nextproc].t_exec);
+            create_process(&proc[nextproc].pid, proc[nextproc].name, nextproc, proc[nextproc].t_exec);
             nextproc ++;
             
             change_priority();
