78051ee8922cc2839bae02c30a1aec9de1784c6d
nataliexn156 <49853522+nataliexn156@users.noreply.github.com>
2019-05-01 02:49:37 +0800

    Update PSJF.c

--- PSJF.c
+++ PSJF.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sched.h>
-
+#include <time.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
@@ -14,7 +14,8 @@
 #define CHILD_CPU 1
 #define PARENT_CPU 0
 
-/* Running one unit time */
+static int last,numberOfTime,run,countFinishing;
+
 #define UNIT_T()				\
 {						\
 	volatile unsigned long i;		\
@@ -44,7 +45,6 @@ int proc_assign_cpu(int pid, int core)
 		perror("sched_setaffinity");
 		exit(1);
 	}
-
 	return 0;
 }
 
@@ -58,189 +58,120 @@ int proc_exec(struct process proc)
 	}
 
 	if (pid == 0) {
-		unsigned long start_sec, start_nsec, end_sec, end_nsec;
 		char to_dmesg[200];
-		syscall(GET_TIME, &start_sec, &start_nsec);
+		unsigned long start_sec, startTime, end_sec, endTime;
+
+		struct timespec t_start, t_finish;
+
+		clock_gettime(CLOCK_REALTIME, &t_start);
+
+		//syscall(GET_TIME, &start_sec, &startTime);
 		for (int i = 0; i < proc.t_exec; i++) {
 			UNIT_T();
-#ifdef DEBUG
-			if (i % 100 == 0)
-				fprintf(stderr, "%s: %d/%d\n", proc.name, i, proc.t_exec);
-#endif
 		}
-		syscall(GET_TIME, &end_sec, &end_nsec);
-		sprintf(to_dmesg, "[project1] %d %lu.%09lu %lu.%09lu\n", getpid(), start_sec, start_nsec, end_sec, end_nsec);
-		syscall(PRINTK, to_dmesg);
+		//syscall(GET_TIME, &end_sec, &endTime);
+		clock_gettime(CLOCK_REALTIME, &t_finish);
+		sprintf(to_dmesg, "[project1] %d %lu.%09lu %lu.%09lu\n", getpid(), t_start.tv_sec, t_start.tv_nsec, t_finish.tv_sec, t_finish.tv_nsec);
+		printf("%s",to_dmesg);
 		exit(0);
 	}
-
-	/* Assign child to another core prevent from interupted by parant */
 	proc_assign_cpu(pid, CHILD_CPU);
 
 	return pid;
 }
 
-int proc_block(int pid)
-{
-	struct sched_param param;
-
-	/* SCHED_IDLE should set priority to 0 */
-	param.sched_priority = 0;
-
-	int ret = sched_setscheduler(pid, SCHED_IDLE, &param);
-
-	if (ret < 0) {
-		//perror("sched_setscheduler");
-		return -1;
-	}
-
-	return ret;
-}
-
 int proc_wakeup(int pid)
 {
 	struct sched_param param;
-
-	/* SCHED_OTHER should set priority to 0 */
 	param.sched_priority = 0;
-
 	int ret = sched_setscheduler(pid, SCHED_OTHER, &param);
-
 	if (ret < 0) {
 		//perror("sched_setscheduler");
 		return -1;
 	}
-
 	return ret;
 }
 
+int proc_block(int pid)
+{
+	struct sched_param param;
+	param.sched_priority = 0;
+	int ret = sched_setscheduler(pid, SCHED_IDLE, &param);
+	if (ret < 0) 
+		return -1;
+	return ret;
+}
 
-
-
-
-/* Last context switch time for RR scheduling */
-static int t_last;
-
-/* Current unit time */
-static int ntime;
-
-/* Index of running process. -1 if no process running */
-static int running;
-
-/* Number of finish Process */
-static int finish_cnt;
-
-/* Sort processes by ready time */
 int cmp(const void *a, const void *b) {
 	return ((struct process *)a)->t_ready - ((struct process *)b)->t_ready;
 }
 
-
-int next_process(struct process *proc, int nproc, int policy)
+int nextP(struct process *proc, int numberOfProcess, int policy)
 {
-	/* Non-preemptive */
 	int ret = -1;
-
-	for (int i = 0; i < nproc; i++) {
+	for (int i = 0; i < numberOfProcess; i++) {
 		if (proc[i].pid == -1 || proc[i].t_exec == 0)
 			continue;
 		if (ret == -1 || proc[i].t_exec < proc[ret].t_exec)
 			ret = i;
 	}
-
-
 	return ret;
 }
 
-
-
-
 int main(int argc, char* argv[])
 {
+	int policy,numberOfProcess;
 	char sched_policy[256];
-	int policy;
-	int nproc;
 	struct process *proc;
 
-	//scanf("%s", sched_policy);
-	scanf("%d", &nproc);
+	scanf("%s", sched_policy);
+	scanf("%d", &numberOfProcess);
 	printf("%s\n", sched_policy);
-	proc = (struct process *)malloc(nproc * sizeof(struct process));
+	proc = (struct process *)malloc(numberOfProcess * sizeof(struct process));
 
-
-	for (int i = 0; i < nproc; i++) {
-
+	for (int i = 0; i < numberOfProcess; i++) 
 		scanf("%s %d %d", proc[i].name,&proc[i].t_ready, &proc[i].t_exec);
 
-	}
+	qsort(proc, numberOfProcess, sizeof(struct process), cmp);
 
-	qsort(proc, nproc, sizeof(struct process), cmp);
-
-	/* Initial pid = -1 imply not ready */
-	for (int i = 0; i < nproc; i++)
+	for (int i = 0; i < numberOfProcess; i++)
 		proc[i].pid = -1;
 
-	/* Set single core prevent from preemption */
 	proc_assign_cpu(getpid(), PARENT_CPU);
-
-	/* Set high priority to scheduler */
 	proc_wakeup(getpid());
-
-	/* Initial scheduler */
-	ntime = 0;
-	running = -1;
-	finish_cnt = 0;
+	numberOfTime = 0;
+	run = -1;
+	countFinishing = 0;
 
 	while (1) {
-		//fprintf(stderr, "Current time: %d\n", ntime);
-
-		/* Check if running process finish */
-		if (running != -1 && proc[running].t_exec == 0) {
-
-			//printf("%s finish at time %d.\n", proc[running].name, ntime);
-			//kill(running, SIGKILL);
-			waitpid(proc[running].pid, NULL, 0);
-			printf("%s %d\n", proc[running].name, proc[running].pid);
-			running = -1;
-			finish_cnt++;
-
-			/* All process finish */
-			if (finish_cnt == nproc)
+		if (run != -1 && proc[run].t_exec == 0) {
+			waitpid(proc[run].pid, NULL, 0);
+			printf("%s %d\n", proc[run].name, proc[run].pid);
+			run = -1;
+			countFinishing++;
+			if (countFinishing == numberOfProcess)
 				break;
 		}
-
-		/* Check if process ready and execute */
-		for (int i = 0; i < nproc; i++) {
-			if (proc[i].t_ready == ntime) {
+		for (int i = 0; i < numberOfProcess; i++) {
+			if (proc[i].t_ready == numberOfTime) {
 				proc[i].pid = proc_exec(proc[i]);
 				proc_block(proc[i].pid);
-				//printf("%s ready at time %d.\n", proc[i].name, ntime);
 			}
 
 		}
-
-		/* Select next running  process */
-
-		int next = next_process(proc, nproc, policy);
-
-
+		int next = nextP(proc, numberOfProcess, policy);
 		if (next != -1) {
-			/* Context switch */
-			if (running != next) {
+			if (run != next) {
 				proc_wakeup(proc[next].pid);
-				proc_block(proc[running].pid);
-				running = next;
-				t_last = ntime;
+				proc_block(proc[run].pid);
+				run = next;
+				last = numberOfTime;
 			}
 		}
-
-		/* Run an unit of time */
 		UNIT_T();
-		if (running != -1)
-			proc[running].t_exec--;
-		ntime++;
-		//printf("ntime %d\n", ntime);
+		if (run != -1)
+			proc[run].t_exec--;
+		numberOfTime++;
 	}
-
 	return 0;
 }
