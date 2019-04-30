f83ed6bc86cdaa880c2720710fde7da197ae6d1c
Curchin <49954678+Curchin@users.noreply.github.com>
2019-04-30 20:19:54 +0800

    60% of the time, it crashes every time

--- FIFO.c
+++ FIFO.c
@@ -4,7 +4,8 @@
 
 int main(int argc, char *argv[]) {
     
-    int N, now;
+    // init
+    int N, nextproc = 0;
     scanf("%d", &N);
     process* proc;
     proc = take_tasks(N);
@@ -12,27 +13,20 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < nproc; i++){
         proc[i].pid = -1;
     }
-        
     
-    int nextproc = 0;
-    
-
-    
+    //F IFO
     for (int time = 0, int i = N; i > 0; t++) {
-        set_priority();
+        priority_ch(proc[nextproc].pid);
         
         while (ptrReady < nProcess && t == R[nextReady]) {
             
             create_process(proc[nextproc].pid, proc[nextproc].name, nextproc, proc[nextproc].t_exec);
             nextproc ++;
-            set_priority();
+            priority_ch(proc[nextproc].pid);
         }
         
-        
-        
+
         run_unit_time();
     }
-    
-    
     exit(0);
 }
